a10257475c7de6087f985ba873299463d438a229
Sergey Vagaytsev <88379648+VagSer@users.noreply.github.com>
2023-02-17 09:00:56 +0300

    Создал кнопку

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
     <h2>Работает</h2>
+    <app-button @click="isLoading=!isLoading">
+        <span class="loader" v-if="isLoading"></span>
+        <span v-else>Создать</span>
+    </app-button>
 </template>
 
 <script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
 
+export default defineComponent({
+    data() {
+        return {
+            isLoading: false
+        }
+    }
+})
 </script>
 
 
 <style scoped>
+.loader {
+    width: 1.5rem;
+    height: 1.5rem;
+    border: 5px solid #FFF;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+    }
+
+    @keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+    } 
 </style>
---
+++ src/components/UI/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class='appButton'>
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+    name: 'app-button'
+})
+</script>
+
+<style>
+.appButton {
+  background: hsl(234, 100%, 69%);
+  color: white;
+  border: none;
+  font-size: 1.5rem;
+  padding: 10px 20px;
+  border-radius: 8px;
+}
+
+.appButton:hover {
+  background: hsl(234, 70%, 69%);
+}
+
+.appButton:disabled {
+  background: hsl(234, 10%, 69%);
+  color: black;
+}
+</style>
